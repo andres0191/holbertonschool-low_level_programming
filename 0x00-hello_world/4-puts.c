efe412eb305e26a102a7191e15414abdad351261
andres0191 <andres_felipe0191@hotmail.com>
2019-09-18 22:57:00 +0000

    modifiy task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{puts("\"Programing is like building a multilingual puzzle");
+{
+puts("\"Programing is like building a multilingual puzzle");
 return (0);
 }
