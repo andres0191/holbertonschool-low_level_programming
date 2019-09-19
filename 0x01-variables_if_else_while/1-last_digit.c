0bc318236a438f1437a7fdebfe4a57778a886f58
andres0191 <andres_felipe0191@hotmail.com>
2019-09-19 21:55:00 +0000

    modify task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 {
 if (n > 5)
 {
