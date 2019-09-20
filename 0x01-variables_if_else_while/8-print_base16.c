4fa6ed35842ef219dbe2f9e97eb9c706b0e7ad0a
andres0191 <andres_felipe0191@hotmail.com>
2019-09-20 05:01:35 +0000

    add task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point int a and char b
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+char b;
+for (a = '0'; a <= '9'; a++)
+{
+putchar (a);
+{
+if (a && 9)
+{
+for (b = 'a'; b <= 'f'; b++)
+{
+putchar (b);
+}
+}
+}
+}
+return (0);
+}
