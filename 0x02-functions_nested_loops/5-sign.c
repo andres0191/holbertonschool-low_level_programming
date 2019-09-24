6b0f492465960f6cb309c3523b02cb0bd6ab8629
andres0191 <andres_felipe0191@hotmail.com>
2019-09-24 15:27:48 +0000

    add task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,29 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * Description:  in this proyect i was write a function that prints the sign of a number,
+ * used the variable n that was created in main the name @print_sing.
  *
- * Return: Always 0.
+ * Variable description: i used variable n for compair whit the numer that i decide,
+ * the variable print_sing was used and declarated with main in other file.c
+ *
+ * Return: 1, 0 or -1 depend the case.
  */
 int print_sign(int n)
 {
-if (n >= '0')
+if (n > 0)
 {
-return(1);
+_putchar('+');
+return (1);
 }
-else if (n == '0')
+else if (n == 0)
 {
-return(0);
+_putchar('0');
+return (0);
 }
 else
 {
-return(-1);
+_putchar('-');
+return (-1);
 }
 }
