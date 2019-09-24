e8a2fb79c691fb7bb6927be3fa7155c19cf6fef7
andres0191 <andres_felipe0191@hotmail.com>
2019-09-24 14:49:46 +0000

    modify task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * is lower - in this proyect wirte a function that checks for lowercase character.
- *i used the variable c but was created in main _islower.
+ * 
+ * Description: i used the variable @c but was created in main @_islower.
  *
  * Return: 1 and 0 depend of the case
  */
@@ -9,11 +9,15 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
+
 	return(1);
+
 	}
 	else
 	{
+
 	return (0);
+
 	}
 _putchar('\n');
 }
