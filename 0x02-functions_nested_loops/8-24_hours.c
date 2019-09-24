998d33dda4cc6e19e5b191c64c0307cda9deefc7
andres0191 <andres_felipe0191@hotmail.com>
2019-09-24 21:28:39 +0000

    modify task 8 for space

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,28 +8,28 @@ void jack_bauer(void)
 int h1, h2, m1, m2, a;
 for (h1 = 0; h1 <= 2; h1++)
 {
-        if (h1 == 2)
-	{
-        a = 3;
-        }
-        else
-        {
-        a = 9;
-        }
-		for (h2 = 0; h2 <= a; h2++)
-	{
-			for (m1 = 0; m1 <= 5; m1++)
-		{
-			for (m2 = 0; m2 <= 9; m2++)
-			{
-				_putchar(h1 + '0');
-				_putchar(h2 + '0');
-				_putchar(':');
-				_putchar(m1 + '0');
-				_putchar(m2 + '0');
-				_putchar('\n');
-			}
-		}
-	}
+if (h1 == 2)
+{
+a = 3;
+}
+else
+{
+a = 9;
+}
+for (h2 = 0; h2 <= a; h2++)
+{
+for (m1 = 0; m1 <= 5; m1++)
+{
+for (m2 = 0; m2 <= 9; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(':');
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}
+}
+}
 }
 }
