7cda0fd4b667b6e81aae2a20ff7c9b08befde908
andres0191 <andres_felipe0191@hotmail.com>
2019-09-25 00:27:55 +0000

    add task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - compared n number with 98
+ * @n: numer of enter
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+if (n < 99)
+{
+for (; n < 99; n++)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+else if (n > 99)
+{
+for (; n > 97; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+}
