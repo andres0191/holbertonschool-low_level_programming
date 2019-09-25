c4b1ca14a36c5547193ccda3e65016d89d4cc528
andres0191 <andres_felipe0191@hotmail.com>
2019-09-25 23:13:22 +0000

    modify task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,33 +7,34 @@
 void times_table(void)
 {
 	int f, c, r, p, u;
+
 	for (c = 0; c <= 9; c++)
 	{
 		for (f = 0; f <= 9; f++)
-		    {
-				r = f * c;
-				if (r == 0)
-				{
-				    _putchar(r +'0');
-				}
-				else if (r >= 10)
-				{
-				    p = r / 10;
-				    u = r % 10;
-				    _putchar(',');
-				    _putchar(' ');
-				    _putchar(p + '0');
-				    _putchar(u + '0');
-				}
-				else
-				{
-				    _putchar(',');
-				    _putchar(' ');
-				    _putchar(' ');
-				    _putchar(r + '0');
-				}
+		{
+			r = f * c;
+			if (r == 0)
+			{
+				_putchar(r + '0');
 			}
+			else if (r >= 10)
+			{
+				p = r / 10;
+				u = r % 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(p + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+		}
 		_putchar('\n');
 	}
-_putchar('\n');
+	_putchar('\n');
 }
