d48044362fa7e6498ad210b874dd27c6e105a5e4
andres0191 <andres_felipe0191@hotmail.com>
2019-09-26 16:03:20 +0000

    add task 2. function that multiplies two integers, and print the result.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/*
+*mul - multiplicate two variable 
+*@a: first number
+*@b: second number
+*Return: result of the multiplicate
+*/
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
