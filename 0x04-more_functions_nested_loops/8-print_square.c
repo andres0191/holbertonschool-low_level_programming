af1ffabb64972a653f7f637dd921e40f3c6f6c24
andres0191 <andres_felipe0191@hotmail.com>
2019-09-27 00:47:05 +0000

    add task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+*
+*
+*/
+
+void print_square(int size)
+{
+int a, b;
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+			_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+		else
+		{
+			_putchar(' ');
+		}
+}
