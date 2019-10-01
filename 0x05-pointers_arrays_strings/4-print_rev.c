38c0285cbdfbf4a45e8c8b16507292bee5e58f59
andres0191 <andres_felipe0191@hotmail.com>
2019-10-01 22:36:23 +0000

    add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_rev - prints a string in reverse
+* @s : variable count string
+**/
+void print_rev(char *s)
+{
+	{int a = 0, y;
+		while (s[a] != 0)
+		{
+			a++;
+		}
+	for (y = a; y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	}
+	_putchar('\n');
+}
