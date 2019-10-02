649187b84b741e192bae78e084d06899f28ccb51
andres0191 <andres_felipe0191@hotmail.com>
2019-10-02 23:30:59 +0000

    modify space in task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,26 +7,26 @@
 void print_triangle(int size)
 {
 int p, q;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (p = 1; p <= size; p++)
-		{
-			for (q = size; q > 0; q--)
-			{
-				if (q <= p)
-				 {
-					 _putchar(35);
-				 }
-				 else
-				 {
-					 _putchar(' ');
-				 }
-			}
-			_putchar('\n');
-		}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (p = 1; p <= size; p++)
+{
+for (q = size; q > 0; q--)
+{
+if (q <= p)
+{
+_putchar(35);
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
 }
