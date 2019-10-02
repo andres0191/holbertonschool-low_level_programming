e314b9c3b31c4d31e526165f47c84bcdaf848386
andres0191 <andres_felipe0191@hotmail.com>
2019-10-02 16:23:53 +0000

    modify task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,14 @@
 **/
 void puts2(char *str)
 {
-int a = 0, b, c;
+int a = 0, b;
 	while (str[a] != 0)
 	{
 		a++;
 	}
-	for (b = 0; b <= 4; b++)
+	for (b = 0; b <= (a - 1); b = b + 2)
 	{
-	c = b * 2;
-	_putchar(c + '0');
+	_putchar(b + '0');
 	}
 	_putchar('\n');
 }
