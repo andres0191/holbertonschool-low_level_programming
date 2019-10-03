cdae844f2e618b102e479c4e5857f6e7325d55b8
andres0191 <andres_felipe0191@hotmail.com>
2019-10-03 03:25:18 +0000

    add task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,18 @@
-#include "holberton.h"
+#include "stdio.h"
 /**
   *
   *
 **/
 void print_array(int *a, int n)
+{int s;
+while (a[n] != 0)
+{
+	n++;
+}
+for (s = 0; s < n; s++)
 {
 
+	printf("%d, ", a[s]);
+}
+putchar('\n');
 }
