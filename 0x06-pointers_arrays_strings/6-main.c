65bce8aea4f6863418e4678647c1fe5de764bda3
andres0191 <andres_felipe0191@hotmail.com>
2019-10-06 21:11:24 +0000

    add task 6

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-   * main - check the code for Holberton School students.
-    *
-     * Return: Always 0.
-      */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
