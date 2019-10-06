139cdf9f395afee97d9d39fbe3ea93e815dbfba6
andres0191 <andres_felipe0191@hotmail.com>
2019-10-06 18:21:30 +0000

    modify comment in task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* string_toupper - -changes all lowercase to uppercase
+* x : pointer and array
+* Return : value of pointer
 **/
+
 char *string_toupper(char *x)
 {
-int a;
-for (a = 0; x[a] != '\0'; a++)
-{
-	if (x[a] <= 122 && x[a] >= 97)
+	int a;
+
+	for (a = 0; x[a] != '\0'; a++)
 	{
-		 x[a] = x[a] - 32;
+		if (x[a] <= 122 && x[a] >= 97)
+		{
+			x[a] = x[a] - 32;
+		}
 	}
-}
-return (x + '0');
+	return (x + '0');
 }
