1f220cebff1160c9b6b6d1edcfd3d0a7bec9a2b3
andres0191 <andres_felipe0191@hotmail.com>
2019-10-13 16:41:53 +0000

    modify comment in task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
+* main - program that prints its name, followed by a new line
+* @argc: count
+* @argv: array
 *
-*
-*
-*
+* Return: Always 0.
 **/
 int main(int argc, char *argv[])
 {
