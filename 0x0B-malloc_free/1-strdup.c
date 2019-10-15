bd8e76761c75d8d9ef6169e8066863e7afc1a608
andres0191 <andres_felipe0191@hotmail.com>
2019-10-15 21:56:34 +0000

    add task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - returns a pointer to a new string
+* @str: pointer of the in
+*
+* Return: Always b
+**/
+char *_strdup(char *str)
+{
+	int a, c;
+	char *b;
+
+	if (str == '\0')
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		;
+	}
+	a++;
+	b = malloc(a * (sizeof(str)));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < a; c++)
+	{
+		b[c] = str[c];
+	}
+	return (b);
+}
