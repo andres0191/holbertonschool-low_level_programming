19589c104b2a29dc86a1db797973a470777c0cf9
andres0191 <andres_felipe0191@hotmail.com>
2019-10-17 23:45:30 +0000

    add task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,4 +10,18 @@
 **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int a, b, c, d;
+	int *p;
+
+	for (a = 0; ptr[a] != '\0'; a++)
+	{
+
+	if (old_size == 0 && ptr != NULL)
+	{
+		return (NULL);
+	}
+	if ( old_size < new_size)
+	{
+
+
 }
