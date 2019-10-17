db2be9b7d86cfc681bca5005de50600deb79c590
andres0191 <andres_felipe0191@hotmail.com>
2019-10-17 23:20:16 +0000

    add task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* realloc -  function that reasigned one block of memory
+* @ptr: pointer of input
+* @old_size: input
+* @new_size: input
+**/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+}
