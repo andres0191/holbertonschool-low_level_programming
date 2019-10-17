b22694ed9f6da8e3b955b27b4b434c5986d9432d
andres0191 <andres_felipe0191@hotmail.com>
2019-10-17 14:43:15 +0000

    add task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,44 +3,19 @@
 #include <stdlib.h>
 
 /**
-  * alloc_grid - returns a pointer to a 2 dimensional array of integer
-  * @width: width of the grid
+  * print_grid - prints a grid of integers
+  * @grid: the address of the two dimensional grid
   * @height: height of the grid
   *
   * Return: Always p
   */
-int **alloc_grid(int width, int height)
+void free_grid(int **grid, int height);
 {
-	int a, b, c, d;
-	int **p;
-	if (width <= 0 || height <= 0)
+	int a, b;
+
+	for (a = 0; a < height; a--)
 	{
-		return (NULL);
+		free(grid[a]);
+		return (grid);
 	}
-	p = malloc(sizeof(int *) * height);
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	for (a = 0; a < height; a++)
-	{
-		p[a] = malloc(sizeof(int) * width);
-			if (p[a] == NULL)
-			{
-				for (b = a; b >= 0; b--)
-				{
-					free(p[b]);
-				}
-				free(p);
-				return (NULL);
-			}
-	}
-	for (c = 0; c < height; c++)
-	{
-		for (d = 0; d < width; d++)
-		{
-			p[c][d] = 0;
-		}
-	}
-	return (p);
 }
