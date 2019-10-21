8fd680bb0132206afd0dbb40ad3e84e1e25fd667
andres0191 <andres_felipe0191@hotmail.com>
2019-10-21 19:31:57 +0000

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * @name : name of input
+  * @age : date of input
+  * @owner : date of input
+  **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
