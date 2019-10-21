5ae6e67760880e650c182967c5bf197123d0cd20
andres0191 <andres_felipe0191@hotmail.com>
2019-10-21 21:56:31 +0000

    add task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,22 +7,17 @@
   **/
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		printf("nil\n");
-	}
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)\n");
-	}
-	if (d == NULL)
-	{
-		printf("\n");
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
-	}
+		}
 }
