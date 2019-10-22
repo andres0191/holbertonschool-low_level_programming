7082bf5fbf39e47f33697ae39f8767e6c4ba091f
andres0191 <andres_felipe0191@hotmail.com>
2019-10-22 00:29:21 +0000

    modify task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 /**
   * free_dog - function that frees dogs
-  * @dog_t: input
   * @d: input
   **/
 void free_dog(dog_t *d)
 {
-	while (d != NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
