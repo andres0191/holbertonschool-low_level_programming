82e177a7d7e31a9e995b7ee0c55e57eb476cc20b
andres0191 <andres_felipe0191@hotmail.com>
2019-10-23 19:01:48 +0000

    add task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,26 @@
-#include "
+#include "function_pointers.h"
+/**
+  * int_index - function that bowser to number int
+  * @array: pointer of input
+  * @size: pointer of input
+  * @cmp: pointer of input
+  *
+  *Return: Always 0
+  **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || (array == NULL || cmp == NULL))
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (0);
+}
