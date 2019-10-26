7a28931e771361d3cbe3688b8adea77d9865536d
andres0191 <andres_felipe0191@hotmail.com>
2019-10-26 17:14:22 +0000

    modify task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**^
+/**
  * struct input - function that compare with the input^
  * @a: char^
  * @fun: fun^
@@ -15,7 +15,6 @@ void print_all(const char * const format, ...);
 typedef struct input
 {
 	char a;
-	void(*fun)(va_list);
+	void (*fun)(va_list);
 } ty;
-
 #endif
