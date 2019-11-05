452198d7b4c593917ae0334452bd0e1050a5c6f7
andres0191 <andres_felipe0191@hotmail.com>
2019-11-05 14:08:30 +0000

    modify code of the file 6-pop free the variable aux

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,7 +8,9 @@
 int pop_listint(listint_t **head)
 {
 	int n = 0;
+	listint_t *aux;
 
+	aux = *head;
 	if (*head == NULL)
 	{
 		return (0);
@@ -17,6 +19,10 @@ int pop_listint(listint_t **head)
 	{
 	n = (*head)->n;
 	*head = (*head)->next;
+	}
+	if (aux != NULL)
+	{
+		free(aux);
 	}
 	return (n);
 }
