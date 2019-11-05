090c95851dcadddcb8980f1ba6f8974a40da0d05
andres0191 <andres_felipe0191@hotmail.com>
2019-11-05 15:03:25 +0000

    modify task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,13 +10,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (head != NULL)
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
 	{
 		for (a = 0; a != index; a++)
 		{
 			head = head->next;
 		}
-		return (head);
 	}
-	return (0);
+		return (head);
 }
