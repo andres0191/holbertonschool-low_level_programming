080915a2942baf659e86850e91f6e3a536fa4df2
andres0191 <andres_felipe0191@hotmail.com>
2019-11-12 21:23:38 +0000

    modify task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
-	if (text_content != NULL)
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
+	else
 	{
 	write(create_file, text_content, strlen(text_content));
 	close(create_file);
