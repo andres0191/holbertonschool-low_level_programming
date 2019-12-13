72dc0d1d1fb18dfc33632bff99e2433f845c0af5
andres0191 <andres_felipe0191@hotmail.com>
2019-12-13 22:57:36 +0000

    add task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+  * get_dnodeint_at_index - function that return the nth node
+  * @head: pointer of input
+  * @index: date of input
+  *
+  * Return: Always 0
+  **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *aux;
+	unsigned int i = 0;
+
+	aux = head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (; i <= index && aux->next != NULL; i++)
+	{
+		if (i == index)
+		{
+			return (aux);
+		}
+		aux = aux->next;
+	}
+	return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * dlistint_t - function that returns the sum of all the data
+  * @head: pointer of input
+  *
+  * Return: Always the sum of data
+  **/
+int sum_dlistint(dlistint_t *head)
+{
+}
