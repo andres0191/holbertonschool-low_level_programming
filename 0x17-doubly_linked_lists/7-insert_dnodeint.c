4e661a281ba6a1be3eb6839c1d04705d7cff9918
andres0191 <andres_felipe0191@hotmail.com>
2019-12-15 04:50:16 +0000

    modify task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	temp = *h;
 	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
+	if (newnode == NULL || h == NULL)
 	{
 		return (NULL);
 	}
