acc9315daae240e7f32867691d05fe30fe7a5ccc
andres0191 <andres_felipe0191@hotmail.com>
2020-01-02 21:19:20 +0000

    monty

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,26 @@
+int c = 0;
+int add(a ,b)
+{
+	c = a + b;
+	return (c);
+}
+int sub(a, b)
+{
+	c = a - b;
+	return (c);
+}
+int mul(a, b)
+{
+	c = a * b;
+	return (c);
+}
+int div(a, b)
+{
+	c = a / b;
+	return (c);
+}
+int mod(a, b)
+{
+	c = a % b;
+	return (c);
+}
