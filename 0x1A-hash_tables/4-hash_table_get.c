91c26598a72a8f2267572bd31e0e019d7f4ad748
andres0191 <andres_felipe0191@hotmail.com>
2020-01-18 20:52:07 +0000

    modify task3

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - function that retrieves a value associated with a key.
+* @ht: pointer of input
+* @key: pointer of input
+*
+* Return: Always char
+**/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	
+}
