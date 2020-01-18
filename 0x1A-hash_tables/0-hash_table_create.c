1dcffb74e34f29b6d817e715394fb8c2f5fdbd87
andres0191 <andres_felipe0191@hotmail.com>
2020-01-18 02:36:04 +0000

    add task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,26 @@
 #include "hash_tables.h"
+
 /**
-*
-*
-*
-*
-*
-**/
+ * hash_table_create - function that create a hash table
+ * @size: data of input, size of has table
+ *
+ * Return: Always temp_table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+	hash_table_t *tempTable = NULL;
+
+	tempTable = malloc(sizeof(hash_table_t));
+	if (tempTable == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	tempTable->size = size;
+	tempTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (tempTable->array == NULL)
+	{
+		free(tempTable);
+		return (NULL);
+	}
+	return (tempTable);
 }
