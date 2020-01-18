357ee4327a4841d7099f02c1fd0d727c73c562d9
andres0191 <andres_felipe0191@hotmail.com>
2020-01-18 20:18:45 +0000

    modify task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,17 +18,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
-	{
-		New_Node->key = strdup(key);
-		New_Node->value = strdup(value);
-		ht->array[index] = New_Node;
-		New_Node->next = ht->array[index];
-	}
 	if (ht->array[index] != NULL)
 	{
 		New_Node->next = ht->array[index];
 		ht->array[index] = New_Node;
 	}
+	if (ht->array[index] == NULL)
+	{
+		New_Node->key = strdup(key);
+		New_Node->value = strdup(value);
+		New_Node->next = ht->array[index];
+		ht->array[index] = New_Node;
+	}
+
 	return (1);
 }
