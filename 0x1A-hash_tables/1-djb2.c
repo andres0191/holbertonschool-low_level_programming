55b1e68f93e434088ff87dca83343a6e54b62bca
andres0191 <andres_felipe0191@hotmail.com>
2020-01-18 02:53:35 +0000

    add task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - function that implementhing the djb2 algorithm
+ * @str: pointer of input
+ * 
+ * Return: Always hash
+ **/
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
@@ -9,28 +16,4 @@ unsigned long int hash_djb2(const unsigned char *str)
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
     return (hash);
-}
-julien@ubuntu:~/0x1A. Hash tables$ 
-julien@ubuntu:~/0x1A. Hash tables$ cat 1-main.c 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "hash_tables.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-    char *s;
-
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    return (EXIT_SUCCESS);
 }
