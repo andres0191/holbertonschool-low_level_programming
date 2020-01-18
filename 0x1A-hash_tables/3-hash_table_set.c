27671290d8a33d71d892ad4311cb58ea9884cbbd
andres0191 <andres_felipe0191@hotmail.com>
2020-01-18 16:27:46 +0000

    modify task 3 and main 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,9 +9,27 @@
 **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    int idx;
-    
+    hash_table_t **Add_Node;
+    hash_node_t *New_Node;
+
+
+
+    *Add_Node = malloc(sizeof(*Add_Node));
+    if (*Add_Node == NULL)
+        return NULL;
+    New_Node = malloc(sizeof(New_Node));
+    if (New_Node == NULL)
+        return NULL;
     if (ht == NULL || key == NULL || value == NULL)
         return 0;
-    
+    number = hash_djb2(key);
+    index = key_index(number);
+    if (Add_Node->array[index] == NULL)
+    {
+        New_Node->key = key;
+        New_Node->value = value;
+        New_Node->next = NULL;
+    }
+    Add_Node->array[index] = New_Node;
+    return (New_Node);
 }
