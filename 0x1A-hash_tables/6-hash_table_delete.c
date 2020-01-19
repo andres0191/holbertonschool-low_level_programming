00a3d215e28159ca6f28bd87ea8e5ff4e7684725
andres0191 <andres_felipe0191@hotmail.com>
2020-01-19 03:21:56 +0000

    betty style task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,27 +2,27 @@
 /**
  * hash_table_delete - function that deltes a hash table
  * @ht: pointer of array
- * */
+ **/
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *aux_delete = NULL;
 
 	if (ht != NULL)
-    {
-        while (i < ht->size)
-        {
-            while (ht->array[i] != NULL)
-		    {
-			    aux_delete = ht->array[i]->next;
-			    free(ht->array[i]->key);
-			    free(ht->array[i]->value);
-			    free(ht->array[i]);
-			    ht->array[i] = aux_delete;
-		    }
-		i++;
-	    }
-	    free(ht->array);
-	    free(ht);
-    }
+	{
+		while (i < ht->size)
+		{
+			while (ht->array[i] != NULL)
+			{
+				aux_delete = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = aux_delete;
+			}
+			i++;
+		}
+		free(ht->array);
+		free(ht);
+	}
 }
