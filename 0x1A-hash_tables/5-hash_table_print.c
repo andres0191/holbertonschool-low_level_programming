2fc791319d135d0d2bcd2ff8573568a2283927d4
andres0191 <andres_felipe0191@hotmail.com>
2020-01-19 02:55:48 +0000

    modify task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,27 +8,18 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0;
 	int bandera = 0;
 
-	if (ht != NULL)
+	printf("{");
+	while (i < ht->size)
 	{
-		printf("{");
-		while (i < ht->size)
+		while (ht->array[i] != NULL)
 		{
-			while (ht->array[i])
-			{
-				if (i > 0 && bandera == 1)
-					printf(", ");
-				printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
-				ht->array[i] = ht->array[i]->next;
-				if (ht->array[i]->next != NULL)
-				{
-					bandera = 0;
-					printf(", ");
-					continue;
-				}
-				bandera = 1;
-			}
-			i++;
+			if (bandera == 1)
+				printf(", ");
+			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			bandera = 1;
+			ht->array[i] = ht->array[i]->next;
 		}
-		printf("}");
+		i++;
 	}
+	printf("}\n");
 }
